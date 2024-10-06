0634b1b674208764761ec837bcdc3bce6d4e2426
WMacalester <willjpmaca@hotmail.com>
2024-10-06 20:14:09 +0100

    feat: first time product generation

---
+++ services/product/graph/resolver.go
@@ -0,0 +1,7 @@
+package graph
+
+import "github.com/WMacalester/GraphKubeL/services/product/graph/model"
+
+type Resolver struct{
+	products []*model.Product
+}
--- services/product/main.go
+++ services/product/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/WMacalester/GraphKubeL/services/product/graph"
+)
+
+const defaultPort = "8080"
 
 func main() {
-	fmt.Println("Product Service")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
